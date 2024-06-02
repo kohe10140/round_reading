ce2509c70367955151fe86b2f05e0827abd44191
kohe10140 <nishiwest.k@gmail.com>
2024-06-02 12:46:06 +0900

    add excercise4_4

--- cracking_the_coding_interview/chapter4/exercise4_4.py
+++ cracking_the_coding_interview/chapter4/exercise4_4.py
@@ -11,38 +11,48 @@ class Solution:
     def __init__(self) -> None:
         pass
 
-    def check(self, root):
+    def isBalanced(self, root):
         if root is None:
             return True
-        none_depth = 1e+10
-        current_depth = 0
+        if not self._isSubBalanced(root):
+            return False
         current = [root]
         while len(current) != 0:
             parents = current
             current = []
-            #breakpoint()
             for parent in parents:
                 if parent.left is not None:
                     current.append(parent.left)
-                else:
-                    none_depth = min(none_depth, current_depth+1)
+                    if not self._isSubBalanced(parent.left):
+                        return False
+
                 if parent.right is not None:
                     current.append(parent.right)
-                else:
-                    none_depth = min(none_depth, current_depth+1)
-                if current_depth - none_depth > 0: # 最も浅いNoneの深さと最も深いノードの深さの差分が１より大きいとFalse
-                    return False
-            current_depth += 1
+                    if not self._isSubBalanced(parent.right):
+                        return False
         return True
+
+    def _get_height(self, node):
+        if node is None:
+            return 0
+        return max(self._get_height(node.left), self._get_height(node.right)) + 1
+
+    def _isSubBalanced(self, node):
+        return abs(self._get_height(node.left)-self._get_height(node.right)) < 2
+        
 
 
 s = Solution()
 
 t1 = TreeNode(0)
-print(s.check(t1))
+print(s.isBalanced(t1))
 t1.left = TreeNode(1)
-print(s.check(t1))
+print(s.isBalanced(t1))
 t1.left.left = TreeNode(2)
-print(s.check(t1))
+print(s.isBalanced(t1))
 t1.right = TreeNode(1)
-print(s.check(t1))
+print(s.isBalanced(t1))
+t1.right.right = TreeNode(2)
+t1.left.left.left = TreeNode(3)
+t1.right.right.right = TreeNode(3)
+print(s.isBalanced(t1))
